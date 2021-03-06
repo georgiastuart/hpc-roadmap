09e6d0eed20bc0560a75f661da19913239709ebd
Georgia Stuart <georgia.stuart@gmail.com>
2021-03-06 16:18:17 -0600

    Add requirements

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import Vuex from 'vuex'
 
-createApp(App).mount('#app')
+// Font Awesome
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+library.add()
+
+createApp(App)
+    .use(router)
+    .use(Vuex)
+    .component('font-awesome-icon', FontAwesomeIcon)
+    .mount('#app')
